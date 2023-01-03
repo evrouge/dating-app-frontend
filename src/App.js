a81a756d97c9759693633069f573513627b701bf
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2023-01-03 17:04:54 -0500

    cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Cards from './components/Cards';
 import Footer from './components/Footer';
 
 
 function App() {
+  // let [people, setPeople] = useState([])
+
+  //   const getPeople = () => {
+  //       axios
+  //           .get('http://localhost:8000/api/dating')
+  //           .then((response) => setPeople(response.data), 
+  //           (err) => console.log(err))
+  //   }
+    
+  //   useEffect (() => {
+  //       getPeople();
+  //   },[])
+
   return (
     <div className="App">
       {/* header */}
       <Header />
       {/* cards */}
-
+      <Cards />
       {/* footer */}
       <Footer />
     </div>
