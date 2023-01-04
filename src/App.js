a95c97b5cabc23e2586fe0d6c10d518e5d0bdf9e
evin <evinlerouge@icloud.com>
2023-01-04 15:06:35 -0700

    worked on front end, css, etc

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/Header';
 import Cards from './components/Cards';
 import Footer from './components/Footer';
+import Home from './components/HomePage';
+import Chat from './components/Chat';
+import Edit from './components/Edit';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
-  // let [people, setPeople] = useState([])
 
-  //   const getPeople = () => {
-  //       axios
-  //           .get('http://localhost:8000/api/dating')
-  //           .then((response) => setPeople(response.data), 
-  //           (err) => console.log(err))
-  //   }
-    
-  //   useEffect (() => {
-  //       getPeople();
-  //   },[])
+  let [people, setPeople] = useState([]);
+
+  const getPeople = () => {
+    axios.get("https://serene-mountain-09515.herokuapp.com/api/dating").then(
+      (response) => setPeople(response.data),
+      (err) => console.log(err)
+    );
+  };
+
+  const handleCreate = (addPerson) => {
+    axios
+      .post("https://serene-mountain-09515.herokuapp.com/api/dating", addPerson)
+      .then((response) => {
+        console.log(response)
+        getPeople();
+      })
+  }
 
   return (
-    <div className="App">
-      {/* header */}
-      <Header />
-      {/* cards */}
-      <Cards />
-      {/* footer */}
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+
+        {/* <Router> */}
+        <Routes>
+          <Route path='/dating/messages' element=
+            {<div><Header /> <Chat /> <Footer /></div>}
+          >
+          </Route>
+          <Route path='/dating/edit' element=
+            {<div><Header /> <Edit /> <Footer /></div>}
+          >
+          </Route>
+          <Route path='/dating' element=
+            {<div><Header /> <Cards getPeople={getPeople} people={people} /> <Footer /></div>}
+          >
+          </Route>
+          <Route path='/' element={<Home getPeople={getPeople} handleCreate={handleCreate} />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Chat() {
+    return (
+        <div className="container">
+            <h1>im the chat page</h1>
+        </div>
+    )
+}
+
+export default Chat;
