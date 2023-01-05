466b387f1a973583d4cba2178738ab4215c76cbb
Emon Li <emonli.328@gmail.com>
2023-01-05 10:58:00 -0500

    removed Footer component from dating route and ported it to Person.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from './components/Header';
-import Cards from './components/Cards';
-import Footer from './components/Footer';
-import Home from './components/HomePage';
-import Chat from './components/Chat';
-import Edit from './components/Edit';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Home from "./components/HomePage";
+import Chat from "./components/Chat";
+import Edit from "./components/Edit";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   let [people, setPeople] = useState([]);
 
   const getPeople = () => {
@@ -31,31 +24,45 @@ function App() {
     axios
       .post("https://serene-mountain-09515.herokuapp.com/api/dating", addPerson)
       .then((response) => {
-        console.log(response)
+        console.log(response);
         getPeople();
-      })
-  }
+      });
+  };
 
   return (
     <Router>
       <div className="App">
-
         {/* <Router> */}
         <Routes>
-          <Route path='/dating/messages' element=
-            {<div><Header /> <Chat /> <Footer /></div>}
-          >
-          </Route>
-          <Route path='/dating/edit' element=
-            {<div><Header /> <Edit /> <Footer /></div>}
-          >
-          </Route>
-          <Route path='/dating' element=
-            {<div><Header /> <Cards getPeople={getPeople} people={people} /> <Footer /></div>}
-          >
-          </Route>
-          <Route path='/' element={<Home getPeople={getPeople} handleCreate={handleCreate} />}>
-          </Route>
+          <Route
+            path="/dating/messages"
+            element={
+              <div>
+                <Header /> <Chat /> <Footer />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/dating/edit"
+            element={
+              <div>
+                <Header /> <Edit /> <Footer />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/dating"
+            element={
+              <div>
+                <Header /> <Cards getPeople={getPeople} people={people} />{" "}
+                {/* <Footer /> */}
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={<Home getPeople={getPeople} handleCreate={handleCreate} />}
+          ></Route>
         </Routes>
       </div>
     </Router>
